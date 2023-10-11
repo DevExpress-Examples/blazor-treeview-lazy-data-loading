1704ecabf31c23e5afd3bebab627ba28ece5462f
elenapeskova <Elena.Peskova@devexpress.com>
2023-10-11 16:32:47 +0400

    add code

---
+++ CS/Data/Northwind/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace DataMappings.Data.Northwind {
+    public partial class Category {
+        public Category() {
+            Products = new HashSet<Product>();
+        }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public byte[] Picture { get; set; }
+
+        [NotMapped]
+        public bool HasProducts { get; set; } = true;
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ CS/Program.cs
@@ -0,0 +1,45 @@
+using DataMappings.Data;
+using DataMappings.Data.Northwind;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddDevExpressBlazor(options => {
+    options.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
+    options.SizeMode = DevExpress.Blazor.SizeMode.Medium;
+});
+builder.Services.AddEntityFrameworkSqlite();
+builder.Services.AddDbContextFactory<NorthwindContext>((sp, options) => {
+    var env = sp.GetRequiredService<IWebHostEnvironment>();
+    var dbPath = Path.Combine(env.ContentRootPath, "Northwind.db");
+    options
+        .UseLazyLoadingProxies()
+        .UseSqlite("Data Source=" + dbPath);
+});
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.WebHost.UseWebRoot("wwwroot");
+builder.WebHost.UseStaticWebAssets();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
